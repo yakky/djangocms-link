31ed11a74c8abc93d851ca54d426de27ac398838
Iacopo Spalletti <i.spalletti@nephila.it>
2016-03-05 03:12:33 +0100

    Use python_2_unicode_compatible from Django, not the CMS

--- djangocms_link/models.py
+++ djangocms_link/models.py
@@ -2,9 +2,9 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.models import CMSPlugin, Page
-from cms.utils.compat.dj import python_2_unicode_compatible
 from django.conf import settings
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .validators import IntranetURLValidator
